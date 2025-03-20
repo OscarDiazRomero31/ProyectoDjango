2991e5bfef0bf3bd23a9039d991feb06dda31e29
Oscar Diaz <oscar.diaz.romero.al@iespoligonosur.org>
2025-03-20 11:16:38 +0100

    Creamos templates con herencias y hemos instalado bootstrap.

--- tienda/tienda/urls.py
+++ tienda/tienda/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path ('',views.index, name = 'inicio'),
     path ('clientes/', views.lista_clientes, name= 'lista_clientes'),
 ]
--- tienda/tienda/views.py
+++ tienda/tienda/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render
 from .models import *
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html', {})
+
 def lista_clientes(request):
     clientes = Cliente.objects.all()
+    #clientes = None //nos mostraria en el template el mensaje de no hay clientes.
     return render(request,'cliente/lista_clientes.html', {'clientes_mostrar': clientes})
 
